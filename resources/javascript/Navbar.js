5b15b2a5cd97592596da9a60897fcf56b94d11b1
Jack D <Jackdefroand@gmail.com>
2021-08-05 21:06:44 +0100

    Managed to get my html and css code looking more cleaner and getting a better grasp of postioning of elements on my pages as well as adding some new styling. Also using markup validation service to clean up any messy HTML code and adding alt tags to images for better web optimization.

--- resources/javascript/Navbar.js
+++ resources/javascript/Navbar.js
@@ -1,15 +1,19 @@
 const navigation = document.querySelector(".navbar");
-const navText = document.getElementById("nav-text");
+const navText = document.querySelector("nav-item");
 
 window.onscroll = function() {scrollFunction()};
 
 const scrollFuction = () => {
   if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    navigation.style.right = "30%";
+    navigation.style.right = "5%";
+    navigation.style.padding = "20px 45px";
+    navText.style.fontSize="0.7em";
   } else {
-    navigation.style.right = "10%";
+    navigation.style.right = "30%";
+    navigation.style.padding = "40px 90px";
+    navText.style.fontSize = "1.5em";
   }
-}
+};
 
 
 
