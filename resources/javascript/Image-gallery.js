72648ea9357e6152eea66d450e2f895a32669742
Jack D <Jackdefroand@gmail.com>
2021-08-06 00:34:43 +0100

    new javascript file created along with html symtax added to make a projects gallery. New image folder and images added

---
+++ resources/javascript/Image-gallery.js
@@ -0,0 +1,18 @@
+const popUp = document.getElementById("popup");
+const gallery = document.getElementById("gallery");
+const selectedImage = document.getElementById("selectedImage");
+const imageIndexes = [1,2,3,4,5];
+const selectedIndex = null;
+
+imageIndexes.forEach((i) => {
+    const image = document.createElement("img");
+    image.src = `../images/preview/projects-${i}.jpg`;
+    image.alt = `This is my project number ${i}`;
+    image.classList.add("galleryImg");
+
+    image.addEventListener("click", () =>{
+        //pop function
+    })
+
+    gallery.appendChild(image);
+});
