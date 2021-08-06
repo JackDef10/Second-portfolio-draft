d57f83b35eb99c70119883d6bd545e06e04f7868
Jack D <Jackdefroand@gmail.com>
2021-08-06 01:37:37 +0100

    images added to projects sections with javascript, next will be working on pop up window for the images

--- resources/javascript/Image-gallery.js
+++ resources/javascript/Image-gallery.js
@@ -1,12 +1,12 @@
-const popUp = document.getElementById("popup");
 const gallery = document.getElementById("gallery");
+const popUp = document.getElementById("popup");
 const selectedImage = document.getElementById("selectedImage");
 const imageIndexes = [1,2,3,4,5];
 const selectedIndex = null;
 
 imageIndexes.forEach((i) => {
     const image = document.createElement("img");
-    image.src = `../images/preview/projects-${i}.jpg`;
+    image.src = `./resources/images/preview/projects-${i}.jpg`;
     image.alt = `This is my project number ${i}`;
     image.classList.add("galleryImg");
 
